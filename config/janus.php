a79bf7837056f018b23aa01193df46222742345e
mrleloi <ledinhloi1997@gmail.com>
2023-12-04 13:11:48 +0700

    add env janus

--- config/janus.php
+++ config/janus.php
@@ -20,8 +20,8 @@ return [
     'main_servers' => [
 //        "wss://windysoft.asia",
 //        "https://windysoft.asia/janus",
-        "wss://janus.windysoft.asia/janus-ws",
-        "https://janus.windysoft.asia/janus/janus",
+        "wss://127.0.0.1/janus-ws",
+        "https://127.0.0.1/janus/janus",
     ],
     'ice_servers' => [
         [
