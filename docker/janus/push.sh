20c95efd698d812a4d9ae3be15e8d26e0154551e
mrleloi <ledinhloi1997@gmail.com>
2023-12-04 12:28:32 +0700

    add janus docker

---
+++ docker/janus/push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+version=`date +%Y%m%d`
+
+echo "push time $version , push version $version"
+docker login -u $1 -p $2
+docker push sucwangsr/janus-webrtc-gateway-docker:$version
+docker logout
